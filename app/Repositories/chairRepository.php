77c4c0adbbe846988ce4f79b607943eb56d778f1
ahmedbenseddiq <benseddiqahmed4@gmail.com>
2025-04-04 18:00:59 +0000

    Chair Crud

---
+++ app/Repositories/chairRepository.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Repositories;
+
+class chairRepository
+{
+    /**
+     * Create a new class instance.
+     */
+    public function __construct()
+    {
+        //
+    }
+}
